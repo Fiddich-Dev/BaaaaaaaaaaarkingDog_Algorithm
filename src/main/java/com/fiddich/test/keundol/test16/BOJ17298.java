70566a490854c718155d580461b269aca9cadf62
Fiddich <78968690+Fiddich-Dev@users.noreply.github.com>
2026-02-23 15:54:46 +0900

    테스트 중간

--- src/main/java/com/fiddich/test/keundol/test16/BOJ17298.java
+++ src/main/java/com/fiddich/test/keundol/test16/BOJ17298.java
@@ -1,4 +1,42 @@
 package com.fiddich.test.keundol.test16;
 
+import java.util.*;
+import java.io.*;
+
 public class BOJ17298 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[] a = new int[n];
+        int[] ret = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Integer> stk = new Stack<>();
+        for(int i = 0; i < n; i++) {
+            int cur = a[i];
+
+            while(!stk.isEmpty() && a[stk.peek()] < cur) {
+                int idx = stk.pop();
+                ret[idx] = cur;
+            }
+
+            stk.push(i);
+        }
+
+        while(!stk.isEmpty()) {
+            ret[stk.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            sb.append(ret[i]).append(" ");
+        }
+
+        System.out.println(sb);
+    }
 }
--- src/main/java/com/fiddich/test/keundol/test16/BOJ5430.java
+++ src/main/java/com/fiddich/test/keundol/test16/BOJ5430.java
@@ -1,4 +1,61 @@
 package com.fiddich.test.keundol.test16;
 
+import java.util.*;
+
 public class BOJ5430 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- != 0) {
+            String s = sc.next();
+            int n = sc.nextInt();
+
+            String ss = sc.next();
+            ss = ss.substring(1, ss.length() - 1);
+            String[] temp = ss.split(",");
+            int[] a = new int[n];
+            for(int i= 0; i < n; i++) {
+                a[i] = Integer.parseInt(temp[i]);
+            }
+
+            solve(a, s);
+        }
+    }
+
+    static void solve(int[] a, String s) {
+        boolean isReverse = false;
+        int l = 0;
+        int r = a.length - 1;
+        for(char c : s.toCharArray()) {
+            if(c == 'R') {
+                isReverse = !isReverse;
+            }
+            if(c == 'D') {
+                if(isReverse) {
+                    r--;
+                }
+                else if(!isReverse) {
+                    l++;
+                }
+
+                if(l > r) {
+                    System.out.println("error");
+                    return;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for(int i = l; i <= r; i++) {
+            if(i == r) {
+                sb.append(a[i]);
+                continue;
+            }
+            sb.append(a[i]).append(",");
+        }
+        sb.append("]");
+        System.out.println(sb);
+    }
 }
